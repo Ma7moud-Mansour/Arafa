853dbadaeadfffb5c5e413cf138dbc899f35b8ea
mahmoud <mahmoudabdelkareem.info@gmail.com>
2026-05-26 05:20:11 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/', // تظبيط مسارات الـ CSS والـ JS عشان النيجينكس يشوفها صح
+  server: {
+    allowedHosts: ['azkararafa.online'] // عشان حظر الـ Vite ميرجعش يضايقك تاني
+  }
 })
